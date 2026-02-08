92a0235bc5e37eee58b96e58e7881007b7ac60d6
Kartik <kartik.mathur10@gmail.com>
2026-02-08 12:42:58 +0530

    Recursion

--- L11-Recursion/4_CheckSorted.cpp
+++ L11-Recursion/4_CheckSorted.cpp
@@ -14,12 +14,38 @@ bool checkSorted(int *a, int n) {
 	return false;
 }
 
+bool checkSorted2(int *a, int n) {
+	if (n == 1) {
+		return true;
+	}
+
+	bool chotaArraySorted = checkSorted2(a, n - 1);
+	if (chotaArraySorted == true && a[n - 1] >= a[n - 2]) {
+		return true;
+	}
+
+	return false;
+}
+
+bool checkSorted3(int *a, int i, int n) {
+	if (i == n - 1) {
+		return true;
+	}
+
+	bool chotaArraySorted = checkSorted3(a, i + 1, n);
+	if (chotaArraySorted == true && a[i] <= a[i + 1]) {
+		return true;
+	}
+
+	return false;
+}
+
 int main() {
 
-	int a[] = {1, 2, 3, -4, 5};
+	int a[] = {1, 2, 3, 4, -5};
 	int n = sizeof(a) / sizeof(int);
 
-	if (checkSorted(a, n)) {
+	if (checkSorted3(a, 0, n)) {
 		cout << "Sorted\n";
 	}
 	else {
