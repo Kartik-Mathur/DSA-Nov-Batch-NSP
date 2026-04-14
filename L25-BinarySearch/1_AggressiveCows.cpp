ac20bc963468c7148f2fbbf93cc8414afa699010
Kartik <kartik@codingblocks.com>
2026-04-14 11:54:19 +0530

    BS

---
+++ L25-BinarySearch/1_AggressiveCows.cpp
@@ -0,0 +1,79 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+#define ll long long
+
+bool isPossible(ll *a,ll n, ll cows, ll d){
+	ll pc = 1, x = a[0];
+
+	for (int i = 1; i < n; ++i)
+	{
+		if(a[i]-x>=d){
+			pc++;
+			x = a[i];
+			if(pc == cows){
+				return true;
+			}
+		}
+	}
+
+	return false;
+}
+
+
+ll aggressiveCows(ll *a,ll n, ll cows){
+	sort(a,a+n);
+
+	ll s =0, e = a[n-1]-a[0];
+	ll ans;
+	while(s<=e){
+
+		ll mid = (s+e)/2;
+
+		if(isPossible(a,n,cows,mid) == true){
+			ans = mid;
+			s = mid + 1;
+		}
+		else{
+			e = mid - 1;
+		}
+	}
+
+	return ans;
+}
+
+
+int main(){
+
+	ll n, cows;
+	ll a[100005];
+
+
+	cin>>n>>cows;
+
+	for (int i = 0; i < n; ++i)
+	{
+		cin>>a[i];
+	}
+
+	cout << aggressiveCows(a,n,cows)<<endl;
+
+
+	return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
